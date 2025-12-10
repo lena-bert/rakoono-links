5fa3f7599dca9a4d6b043cdf5f587d24ddbbf7f6
lena-bert <lena@rakoono.com>
2025-12-10 17:50:11 +0100

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Hello world from Berlin!
+      </h1>
+    </main>
+  );
+}
